3af9969b6ec9983436133c4e0981b8acdc87a16b
ediszz <edvinas.riepsas@gmail.com>
2021-04-02 20:52:14 +0300

    death

---
+++ resources/views/gameView.blade.php
@@ -0,0 +1,30 @@
+@extends('layout')
+
+@section('content')
+
+    <div class="container">  {{--canvas --}}
+       <div class="row">
+           <canvas style="width: 100%; text-align: center; display: inline; border: solid #1a202c 5px; margin-top: 5px">
+                ???
+           </canvas>
+       </div>
+        <div class="row" style="margin-top: 10px">
+            <div class="col-6" style="border: solid #1a202c 5px">
+                Apie zaidima
+            </div>
+            <div class="col-1" style="border: solid #1a202c 5px; margin-left: 5px">
+                Rate
+            </div>
+        </div>
+        <div class="row" style="border: solid #1a202c 5px; margin-top: 10px; margin-bottom: 10px">
+            Leaderboard
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+        </div>
+    </div>
+@endsection
