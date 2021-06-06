5cb311f72f78ad9647afd856bc86623d3eb7b91c
edvrie <61147298+edvrie@users.noreply.github.com>
2021-06-06 18:59:32 +0300

    More refactoring

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>ScuffedY8.5</title>
+    <title>BEEGames</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="{{asset('css/app.css?v=').time()}}">
